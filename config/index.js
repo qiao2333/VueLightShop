c571dc45567dfed6a3e5618494f33be4081e3aa6
qiaoge2333 <1170793857@qq.com>
2019-06-06 09:23:06 +0800

    Update index.js
    改成localhost

--- config/index.js
+++ config/index.js
@@ -13,7 +13,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api': {
-        target: "http://120.79.163.179:8080",
+        target: "http://localhost:8080",
         changeOrigin: true,
         pathRewrite: {
           '^/api': ''
